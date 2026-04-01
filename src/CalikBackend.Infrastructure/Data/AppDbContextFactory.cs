69c371704d3e2dcb6b984d5fda5133ddadf3b477
gurkanmaral <gurkan.maral@eczacibasi.com.tr>
2026-04-01 16:19:01 +0300

    fix

--- src/CalikBackend.Infrastructure/Data/AppDbContextFactory.cs
+++ src/CalikBackend.Infrastructure/Data/AppDbContextFactory.cs
@@ -12,6 +12,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: true)
             .AddJsonFile("appsettings.Development.json", optional: true)
+            .AddJsonFile("appsettings.Production.json", optional: true)
             .Build();
 
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
