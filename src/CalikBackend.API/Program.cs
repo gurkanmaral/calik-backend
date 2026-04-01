74e9eafd5d8da2c277d6967113b5ff4958259a80
gurkanmaral <gurkan.maral@eczacibasi.com.tr>
2026-04-01 17:55:10 +0300

    fix

--- src/CalikBackend.API/Program.cs
+++ src/CalikBackend.API/Program.cs
@@ -19,8 +19,9 @@ builder.Services.AddProblemDetails();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
-        policy.WithOrigins("http://localhost:5173", "http://localhost:5174")
+        policy
               .AllowAnyHeader()
+              .AllowAnyOrigin()
               .AllowAnyMethod()
               .AllowCredentials());
 });
